18f2aad7f0ac1d2bb1fad8f6f712246e9d8f0ad4
codythegreat <maxiecody@gmail.com>
2022-01-29 11:50:47 -0600

    further refinement - 1/29/2022

--- public/convert-country-alpha-codes.py
+++ public/convert-country-alpha-codes.py
@@ -1,11 +1,9 @@
-# file used to convert 2 letter alpha codes to 3 letter on svg in index.html 
-
 import csv
 from bs4 import BeautifulSoup
 
 
-country_alpha_codes = open('country-alpha-codes.csv')
-csvreader = csv.reader(country_alpha_codes)
+country_data = open('../Human_life_Expectancy.csv')
+csvreader = csv.reader(country_data)
 
 rows = []
 for row in csvreader:
@@ -18,10 +16,16 @@ page = open(url)
 
 soup = BeautifulSoup(page.read(), features="lxml")
 
-for el in soup.find_all(lambda e: e.name == 'path' and e.attrs.get('id') != None):
+for el in soup.find_all(lambda e: e.name == 'path'):
+    found = False
     for row in rows:
-        if row[1] == el['id']:
-            el['id'] = row[2]
+        if el.has_attr('class'): 
+            found = row[0] == ' '.join(el['class'])
+        elif el.has_attr('name'):
+            found = row[0] == el['name']
+        if found: break
+    if not found:
+        el['fill'] = '#bcbcbc'
 
 with open("__index.html", "w", encoding="utf-8") as file:
     file.write(str(soup))
